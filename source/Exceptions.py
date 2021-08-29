d2479b85f34d3bc7d2ae3cd341977272e787b7c3
Pedro Q <pdqueiros@gmail.com>
2021-08-29 18:13:16 +0200

    changed km to actually represent kegg matrix completeness; added verbose mode
    Former-commit-id: 6533ab7ba0437572f0ece004a5af26365ba84c7f [formerly 231a762cfa74dd7cbaac9d7d6183fd2d9666a1d3]
    Former-commit-id: 59b4a9daf860bf92e2498aca09aa2d88c9d43f15

--- source/Exceptions.py
+++ source/Exceptions.py
@@ -1,6 +1,7 @@
 RequirementsNotMet='Installation check not passed! Make sure you\'ve setup the databases and your system meets all the requirements!'
 NoValidFiles='No valid files to annotate'
 InvalidTargetFile='You did not insert a valid target file!\n'
+InvalidFastaFormat='Fasta format is not valid!\n'
 InstallationCheckNotPassed='Installation check not passed! Make sure you\'ve setup the databases and your system meets all the requirements!'
 CythonNotCompiled= 'Cython has not been correctly compiled! Please go to mantis/source/ and run python utils.py'
 BadNumberWorkers='You should not be seeing this, please contact the developer. Invalid number of workers in '
